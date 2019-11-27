f4820bdad3bd0ce0e9d80a0c6774a80a638ebdac
Paul Austen <pausten.os@gmail.com>
2019-11-27 19:46:09 +0000

    Update template to provide the ws package.

---
+++ README.md
@@ -0,0 +1,36 @@
+# A Basic Python web server
+A simple web server for I used for developing a web interface to an embedded 
+device. 
+
+# Installation
+
+## Debian package
+
+- Ensure the p3build package is installed. See [https://github.com/pjaos/p3build](https://github.com/pjaos/p3build) for details of this.
+- Run `sudo p3build` in this folder.
+- Run `sudo dpkg -i packages/python-ws-1.0-all.deb`
+
+## Using pip
+
+- Run the `sudo pip3 install .` command.
+
+# Running 
+
+- Run `ws -h`
+
+and the help text is displayed
+
+```
+Usage: Simple web server.
+
+Options:
+  -h, --help   show this help message and exit
+  --port=PORT  Followed by the web server port (default=8080)
+  --root=ROOT  Followed by the web server root path
+  --cgi=CGI    A folder (in the root path) containing the cgi scripts
+               (default=/cgi-bin).
+
+```
+
+The `--root` path must be provides an should point to the web server root folder.
+The `--cgi` folder is optional.
---
+++ python/ws.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import socketserver
+import http.server
+import http.server
+import logging
+import cgi
+from   optparse import OptionParser
+from   os import chdir
+import cgitb; 
+
+VERSION = 1.6
+
+class ServerHandler(http.server.CGIHTTPRequestHandler):
+
+    QUERY_STRING = "QUERY_STRING"
+    
+    def do_GET(self):
+        logging.warning("======= GET STARTED =======")
+        logging.warning(self.headers)
+        http.server.CGIHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        logging.warning("======= POST STARTED =======")
+        logging.warning(self.headers)
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+        logging.warning("======= POST VALUES =======")
+        for item in form.list:
+            logging.warning("%s=%s" % (item.name, item.value) )
+        logging.warning("\n")
+        http.server.CGIHTTPRequestHandler.do_POST(self)
+   
+def shutdown(server):
+    """Shutdown the web server"""
+    if server != None:
+        server.socket.close()
+        print(("Shutdown server on port", port))
+
+def main():
+    opts=OptionParser(usage='Simple web server.')
+    opts.add_option("--port", help="Followed by the web server port (default=8080)", type="int", default=8080)
+    opts.add_option("--root", help="Followed by the web server root path", default=".")
+    opts.add_option("--cgi",  help="A folder (in the root path) containing the cgi scripts (default=/cgi-bin).", default="/cgi-bin")
+    server = None
+    port=None
+    try:
+        (options, args) = opts.parse_args()
+        
+        chdir(options.root)
+        
+        cgitb.enable()
+        
+        Handler = ServerHandler
+        port    = options.port
+        
+        Handler.cgi_directories = [options.cgi]
+
+        print(("serving at port", options.port))
+        socketserver.TCPServer.allow_reuse_address = True
+        socketserver.ThreadingTCPServer.allow_reuse_address = True
+        server = http.server.HTTPServer(("", options.port), Handler)
+
+        server.serve_forever()
+
+    #If the user presses CTRL C
+    except KeyboardInterrupt:
+        shutdown(server)
+
+    #If the program throws a system exit exception
+    except SystemExit:
+        shutdown(server)
+      
+    except:
+      raise
+
+if __name__ == '__main__':
+    main()
